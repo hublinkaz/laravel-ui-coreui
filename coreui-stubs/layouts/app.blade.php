e37c070bb65156ecf11f714e3b83ab7f623c13e1
hublinkaz <88090171+hublinkaz@users.noreply.github.com>
2022-04-21 01:45:42 -0700

    son 3

--- coreui-stubs/layouts/app.blade.php
+++ coreui-stubs/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
           integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
           crossorigin="anonymous"/>
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
     @yield('third_party_stylesheets')
 
@@ -39,7 +40,6 @@
 
 <script src="{{ mix('js/app.js') }}" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
